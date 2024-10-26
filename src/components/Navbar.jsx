6a8911fbc3b84d4fc5ceb3a76e94f4e3c064055c
suhani tamrkar <suhanitamrkar2004@gmail.com>
2024-10-26 22:31:27 +0530

    learn some basic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 
+function Item({name,isPacked}){
+return (
+    // <li>{isPacked ? (<del>{name + ' ✅'} </del>   ): (name)} </li>
+    <li>{name} {isPacked && ' ✅'} </li>
+)
+ 
+    // if (isPacked) {
+    //     return <li className='item'>{name} </li>        
+    //         }
+}
+
 const Navbar = () => {
+   
   return (
-    <div>this is navabr</div>
+    <div>
+<h1>Sally Ride's Packing List</h1>
+<ul>
+    
+    <Item isPacked={true} name="space suit" />
+    <Item isPacked={true} name="Helmet with a golden leaf" />
+    <Item isPacked={false} name="Photo of tam" />
+
+</ul>
+    </div>
   )
 }
 
